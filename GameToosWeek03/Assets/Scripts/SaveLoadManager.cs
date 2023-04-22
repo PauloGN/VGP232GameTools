d1d5053887190984087154ea37f4408c3d0328d9
Paulo Roberto dos Santos <paulorsantos.gnt@outlook.com>
2023-04-21 17:10:37 -0700

    Save load manager

--- GameToosWeek03/Assets/Scripts/SaveLoadManager.cs
+++ GameToosWeek03/Assets/Scripts/SaveLoadManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public class SaveLoadManager : MonoBehaviour
 {
@@ -11,14 +12,46 @@ public class SaveLoadManager : MonoBehaviour
     void Update()
     {
 
-        if(Input.GetKeyDown(KeyCode.S))
+        if (Input.GetKeyDown(KeyCode.S))
         {
             SaveDataBinaryM.Save(gd);
         }
-        if(Input.GetKeyDown(KeyCode.L))
+        if (Input.GetKeyDown(KeyCode.L))
         {
             gd = SaveDataBinaryM.Load();
         }
+    }
+}
+
+[CustomEditor(typeof(SaveLoadManager))]
+public class SaveAndLoad : Editor
+{
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        SaveLoadManager saveUI = (SaveLoadManager)target;
+        if (GUILayout.Button("Serialize to Binary Data"))
+        {
+            SaveDataBinaryM.Save(saveUI.gd);
+        }
+
+        if (GUILayout.Button("Load Binary data"))
+        {
+            SaveDataBinaryM.Load();
+        }
+
+        if (GUILayout.Button("Serialize to json Data"))
+        {
+            SaveDataManager.Save(saveUI.gd);
+        }
+
+        if (GUILayout.Button("Load json data"))
+        {
+            SaveDataManager.Load();
+        }
 
     }
+
 }
